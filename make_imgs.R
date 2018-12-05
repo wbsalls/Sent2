d0dff620ac00400dfc307571385e9b4edf0e5f50
wbsalls <7197767+wbsalls@users.noreply.github.com>
2018-12-05 17:57:45 -0500

    major progress on chlorophyll and trophic state mapping

--- make_imgs.R
+++ make_imgs.R
@@ -30,30 +30,42 @@ for (i in seq_along(mci_files_jlake)) {
 
 
 # set image folder paths
-img_folder <- "C:/Users/WSalls/Desktop/s2_imgs_agu/mci/jordan" #jordan or utah
+#img_folder <- "C:/Users/WSalls/Desktop/s2_imgs_agu/mci/jordan" #jordan or utah
+img_folder <- "/Users/wilsonsalls/Desktop/EPA/Presentations/AGU2018/data/mci/jordan"
 imgs <- list.files(img_folder, pattern = ".data")
 
 # load lake shp
 library(rgdal)
 lakes <- readOGR("O:/PRIV/NERL_ORD_CYAN/Salls_working/geospatial_general/resolvableLakes/NHD_NLA_shoredist", 
                  "nhd_nla_subset_shore_dist")
+lakes <- readOGR("/Users/wilsonsalls/Desktop/EPA/geosp_general/resolvableLakes/NHD_NLA_shoredist",
+                 "nhd_nla_subset_shore_dist")
+
+library(raster)
 
 # select jordan lake; reproject to UTM for use with rasters, loading a raster first to get CRS
-jordan <- lakes[which(lakes$COMID == 166755060), ]
+lake_poly_raw <- lakes[which(lakes$COMID == 166755060), ] #166755060 for jordan; xx for utah
 rast <- raster(file.path(img_folder, imgs[1], "MCI.img"))
-jordan_utm <- spTransform (jordan, crs(rast))
+lake_poly <- spTransform (lake_poly_raw, crs(rast))
 
+##
 
-# remove edges
+## clip, remove edges, convert to chlorophyll, save new rasters ---------------
+
+lakename <- "jordan" # jordan OR utah
+
+rast_out_dir <- file.path("/Users/wilsonsalls/Desktop/EPA/Presentations/AGU2018/data/mci_cropped/", lakename)
+
 for (i in seq_along(imgs)) {
-  print(sprintf("image %s of %s", i, length(imgs)))
+  idate <- substr(imgs[i], 27, 34)
+  print(sprintf("image %s of %s: %s", i, length(imgs), idate))
   
   # load raster
   rast <- raster(file.path(img_folder, imgs[i], "MCI.img"))
   
   # clip to lake
-  rast_clip <- mask(rast, jordan_utm)
-  rast_crop <- crop(rast_clip, jordan_utm)
+  rast_mask <- mask(rast, lake_poly) # raster values
+  rast_crop <- crop(rast_mask, lake_poly) # raster extent
   
   # remove edges
   rast_crop <- focal(rast_crop, matrix(c(0,0,0,0,1,0,0,0,0), nrow = 3))
@@ -64,9 +76,94 @@ for (i in seq_along(imgs)) {
   # remove negative chl
   values(rast_crop)[which(values(rast_crop) < 0)] <- NA
   
-  # plot
-  #jpeg(sprintf("jordan_%s.png", substr(imgs[i], 27, 34)), width = 800, height = 860)
-  plot(rast_crop, main = substr(imgs[i], 27, 34), xaxt = "n", yaxt = "n", bty = "n")
-  #plot(jordan_utm, add = TRUE)
-  #dev.off()
+  # write raster
+  writeRaster(rast_crop, file.path(rast_out_dir, 
+                                   sprintf("chlorophyll_%s_%s.png", lakename, idate)), "GTiff")
 }
+
+
+## --------------------------------------------------------
+
+
+### plot
+
+# set location to save images
+setwd("/Users/wilsonsalls/Desktop/EPA/Presentations/AGU2018/imgs")
+
+# set location to read rasters from
+chl_rasts <- list.files(rast_out_dir)
+
+
+## plot chlorophyll, save ---------------
+
+# preset min and max
+#min <- NA
+#max <- NA
+
+# plot
+for (i in seq_along(imgs)) {
+  idate <- substr(imgs[i], 27, 34)
+  print(sprintf("image %s of %s: %s", i, length(imgs), idate))
+  
+  # read raster
+  rast_crop <- raster(file.path(rast_out_dir, chl_rasts[i]))
+  
+  # plot
+  jpeg(sprintf("chl_%s_%s.png", lakename, idate), width = 600, height = 1200)
+  max.color.val <- 100
+  plot(rast_crop,
+       main = paste0(substr(idate, 1, 4), "-", substr(idate, 5, 6), "-", substr(idate, 7, 8)),
+       cex.main = 4,
+       xaxt = "n", yaxt = "n", box = FALSE, bty = "n",
+       #col = colorRampPalette(c("blue", "green", "yellow"))(255),
+       col = viridis(max.color.val),
+       breaks = seq(1, max.color.val, length.out = max.color.val),
+       colNA = "black")
+  dev.off()
+  
+  # get min and mox to improve plotting
+  #min <- min(min, minValue(rast_crop), na.rm = T)
+  #max <- max(max, maxValue(rast_crop), na.rm = T)
+}
+
+min # ~0
+max # 117.3
+
+#
+
+## plot trophic state, save ---------------
+
+# plot
+for (i in seq_along(imgs)) {
+  idate <- substr(imgs[i], 27, 34)
+  print(sprintf("image %s of %s: %s", i, length(imgs), idate))
+  
+  # read raster
+  rast_crop <- raster(file.path(rast_out_dir, chl_rasts[i]))
+  
+  # plot
+  jpeg(sprintf("trophic_%s_%s.png", lakename, idate), width = 600, height = 1200)
+  max.color.val <- 120
+  plot(rast_crop, 
+       main = paste0(substr(idate, 1, 4), "-", substr(idate, 5, 6), "-", substr(idate, 7, 8)),
+       cex.main = 4,
+       xaxt = "n", yaxt = "n", box = FALSE, bty = "n",
+       col = plasma(4),
+       breaks = c(0, 2, 7, 30, max.color.val),
+       colNA = "black")
+  dev.off()
+  
+}
+
+# colors used
+plasma(4)
+
+#
+
+
+
+showMethods("plot")
+getMethod("plot", c("Raster", "ANY"))
+getAnywhere(".plotraster2")
+getAnywhere(".rasterImagePlot")
+args(raster:::.rasterImagePlot)
